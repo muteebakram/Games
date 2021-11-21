8f22cdc35ed5fb95f4476318dfa38aee29d3d0b5
Muteeb Nawaz <mutnawaz@cisco.com>
2021-11-21 17:28:31 +0530

    Small fixes Tic Tac Toe

--- TicTacToe/app.py
+++ TicTacToe/app.py
@@ -169,9 +169,9 @@ class TicTacToe:
         self.print_points()
 
         if self.player_o_wins > self.player_x_wins:
-            print("Player-X is Tic Tac Toe champion.")
-        elif self.player_x_wins > self.player_o_wins:
             print("Player-O is Tic Tac Toe champion.")
+        elif self.player_x_wins > self.player_o_wins:
+            print("Player-X is Tic Tac Toe champion.")
         else:
             print("Draw! Players you need to re-match for ultimate Tic Tac Toe champion.")
 
